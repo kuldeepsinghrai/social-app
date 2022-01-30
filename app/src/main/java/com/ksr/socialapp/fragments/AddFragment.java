5ecb80d4217c60769b5ffdd924662306e48a1ea4
Kuldeep Singh Rai <imkuldeepsinghrai@gmail.com>
2022-01-30 18:49:47 +0530

    AddFragment Design improved

--- app/src/main/java/com/ksr/socialapp/fragments/AddFragment.java
+++ app/src/main/java/com/ksr/socialapp/fragments/AddFragment.java
@@ -1,5 +1,7 @@
 package com.ksr.socialapp.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -9,21 +11,43 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ksr.socialapp.R;
+import com.ksr.socialapp.model.User;
+import com.makeramen.roundedimageview.RoundedImageView;
+import com.squareup.picasso.Picasso;
 
 public class AddFragment extends Fragment {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
 
     private Button postBT;
     private EditText postDescriptionET;
     private ImageView postImage, addImage;
+    private RoundedImageView profileImage;
+    private TextView name, profession;
+    private Uri uri;
 
     public AddFragment() {
         //Required Empty Constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
     }
 
     @Nullable
@@ -31,10 +55,34 @@ public class AddFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add, container, false);
 
+
         postBT = view.findViewById(R.id.postBT);
         postDescriptionET = view.findViewById(R.id.postDescriptionET);
         postImage = view.findViewById(R.id.postImage);
         addImage = view.findViewById(R.id.addImage);
+        profileImage = view.findViewById(R.id.profileImage);
+        name = view.findViewById(R.id.name);
+        profession = view.findViewById(R.id.profession);
+
+        firebaseDatabase.getReference().child("Users")
+                .child(FirebaseAuth.getInstance().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()){
+                            User user = snapshot.getValue(User.class);
+                            Picasso.get().load(user.getProfile()).placeholder(R.drawable.placeholder).into(profileImage);
+                            name.setText(user.getName());
+                            profession.setText(user.getProfession());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
 
 
         postDescriptionET.addTextChangedListener(new TextWatcher() {
@@ -45,7 +93,11 @@ public class AddFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                
+                if (postDescriptionET.getText().toString().length() > 0) {
+                    postBT.setEnabled(true);
+                } else {
+                    postBT.setEnabled(false);
+                }
             }
 
             @Override
@@ -55,8 +107,34 @@ public class AddFragment extends Fragment {
         });
 
 
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 101);
+            }
+        });
+
+
         return view;
 
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            if (data.getData() != null) {
+                uri = data.getData();
+                postImage.setImageURI(uri);
+                postBT.setEnabled(true);
+            } else {
+                postBT.setEnabled(false);
+            }
+
+        }
     }
 }
